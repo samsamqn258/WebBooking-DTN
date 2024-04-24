646719fbf97be52d5a8dafbc99e141d664e13d00
Nam nguyen <012003hoangnam@gmail.com>
2024-04-24 09:50:02 +0700

    updateDB

---
+++ Models/Envaluate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBooking.Models
+{
+    public class Envaluate
+    {
+        [Key]
+        public int EnvaluateID { get; set; }
+        public int PaymentID { get; set; }
+        public string? Comment { get; set; }
+        public int? Score { get; set; }
+        public DateTime EnvaluateTime {  get; set; }
+        public string? Image1 { get; set; }
+        public string? Image2 { get; set; }
+        [ForeignKey("PaymentID")]
+
+        public Payment? Payment { get; set; }
+
+
+    }
+}
